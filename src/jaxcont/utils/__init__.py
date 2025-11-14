b8086f7f993ba4899a30bfe43a5aabf12dae21af
Abolfazl  Ziaeemehr <a.ziaeemehr@gmail.com>
2025-11-14 17:40:06 +0100

    Refactor __init__.py to include additional utility functions in the module exports

--- src/jaxcont/utils/__init__.py
+++ src/jaxcont/utils/__init__.py
@@ -1,6 +1,22 @@
 """Utility functions and helpers."""
 
-from jaxcont.utils.config import Config
+from jaxcont.utils.config import (
+    Config,
+    test_jax_cuda,
+    print_jax_cuda_info,
+    test_package_imports,
+    print_package_import_test,
+    run_installation_tests,
+)
 from jaxcont.utils.plotting import plot_bifurcation_diagram, plot_continuation
 
-__all__ = ["Config", "plot_bifurcation_diagram", "plot_continuation"]
+__all__ = [
+    "Config",
+    "plot_bifurcation_diagram",
+    "plot_continuation",
+    "test_jax_cuda",
+    "print_jax_cuda_info",
+    "test_package_imports",
+    "print_package_import_test",
+    "run_installation_tests",
+]
